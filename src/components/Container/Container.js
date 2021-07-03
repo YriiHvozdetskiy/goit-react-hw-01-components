526e51fc5c7a18031c1be54db9881824e2e316a5
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-07-03 12:52:43 +0300

    test

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,7 +1,7 @@
 import s from './Container.module.css';
 
 const Container = ({ children }) => {
-  return <div className={ss.container}>{children}</div>;
+  return <div className={sff.container}>{children}</div>;
 };
 
 export default Container;
